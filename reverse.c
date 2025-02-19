ffc7654f3bb26dded906245e4e3ab8a1dc135183
Lakshmy1907 <lakshmybyju2028@cs.sjcetpalai.ac.in>
2025-02-19 14:51:48 +0530

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+int leng;
+char str[20],temp;
+printf("\n enter the string")
+scanf("%s",str[20];
+for(len=0;str1[len]!='\0';len++){
+int i=len-1;
+for(int j=0;j<len/2;j++){
+temp=str[i];
+str[i]=str[j];
+str[j]=temp;
+i--;
+ }
+}
+printf("the reveres string is");
+gets(str);
+return 0;
+}
+
+
+
