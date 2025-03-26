194ca12596ec984d85027a763a561712e24450ee
Lakshmy1907 <lakshmybyju2028@cs.sjcetpalai.ac.in>
2025-03-26 16:23:28 +0530

    Add files via upload

---
+++ exponent.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+// Function to calculate exponent
+int exponent(int n1, int n2) {
+    if (n2 == 0) {
+        return 1;
+    } else {
+        return n1 * exponent(n1, n2 - 1);
+    }
+}
+
+int main() {
+    int n1, n2;
+    printf("Enter the number: ");
+    scanf("%d", &n1);
+    printf("Enter the power: ");
+    scanf("%d", &n2);
+    
+    // Call the exponent function and store the result
+    int result = exponent(n1, n2);
+    
+    // Print the result
+    printf("Result: %d\n", result);
+    return 0;
+}
+
